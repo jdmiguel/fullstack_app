d21a56fd1de2a52168cb977b27f795942b2f383f
Jaime de Miguel Alcobendas <Jaime.Miguel@i3television.es>
2018-08-09 14:44:12 +0200

    index.js updated right now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({hi: 'there from heroku and github connected'});
+    res.send({hi: 'there from heroku and github connected right now'});
 });
 
 const PORT = process.env.PORT || 5000;
