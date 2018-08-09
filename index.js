73a9c016a4f936289c3189b5c0c2bcc19349c33b
Jaime de Miguel Alcobendas <Jaime.Miguel@i3television.es>
2018-08-09 14:48:12 +0200

    npm script deploy created for syncro git push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({hi: 'there from heroku and github connected right now'});
+    res.send({hi: 'syncronized'});
 });
 
 const PORT = process.env.PORT || 5000;
