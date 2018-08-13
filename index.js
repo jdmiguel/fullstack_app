6ae7f8683848d7d3169e8ac88287e1a3d0e16342
Jaime de Miguel Alcobendas <Jaime.Miguel@i3television.es>
2018-08-13 10:24:54 +0200

    index.js updated 13082018

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send({hi: 'fullstack changed'});
+    res.send({hi: 'fullstack changed in this moment'});
 });
 
 app.get('/greet', (req,res) => {
