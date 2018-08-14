21ef1f1c9ed0ac7f2daf653425f3dca612ae0bc1
jdmiguel <jaimedmiguel@gmail.com>
2018-08-14 14:41:49 +0200

    client id createed in google

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({hi: 'fullstack changed now'});
-});
 
-app.get('/greet', (req,res) => {
-    res.send({hi: 'greeting!'});
-});
+// clientID 624435429462-3qb4fnvm8i2jgilea27t179ge0ded2jp.apps.googleusercontent.com
+// clientSecret 8zoXoMauUjsrPYBNqSC0lgzA
+
+passport.use(new GoogleStrategy());
 
 
 const PORT = process.env.PORT || 5000;
