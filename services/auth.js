72683c389d86c422b3888c1d5934e70db6bf3a05
jdmiguel <jaimedmiguel@gmail.com>
2018-08-19 14:45:11 +0200

    creating new collection 'users' with mongoose

--- services/auth.js
+++ services/auth.js
@@ -1,17 +1,21 @@
+const mongoose = require('mongoose');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //const FacebookStrategy = require('passport-facebook').Strategy;
 
 const keys = require('../config/keys'); 
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken,refreshToken,profile) => {
-    console.log('accessToken',accessToken)
+    /*console.log('accessToken',accessToken)
     console.log('refreshToken',refreshToken)
-    console.log('profile',profile)
+    console.log('profile',profile)*/
+    new User({ googleId: profile.id }).save();
+
 }));
 
 /*
